0d51f7a10702aff5d28900bf1a42cf04801cb2e1
Anderson Sachetto Rosa <sachetto@gmail.com>
2023-06-11 20:40:34 -0300

    New implementations

--- infra/Repository/Share_Repository.py
+++ infra/Repository/Share_Repository.py
@@ -6,3 +6,33 @@ class ShareRepository:
         with DBConnectionHandler() as db:
             data = db.Section.query(Share).all()
             return data
+        
+    def SelectOperationsFrom(self, symbol):
+        with DBConnectionHandler() as db:
+            rows = db.Section.query(Share)\
+                .filter(Share.ticker == symbol).all()
+            return rows
+        
+    def SelectDistinctSymbols(self):
+        with DBConnectionHandler() as db:
+            data = db.Section.query(Share.ticker).distinct().all()
+            
+            # Get the distinct values as a list using list comprehension
+            data = [value[0] for value in data]
+            return data
+   
+    def SelectDistinctSymbolsAndName(self):
+        with DBConnectionHandler() as db:
+            data = db.Section.query(Share.ticker, Share.titulo)\
+                .distinct().all()
+            
+            # Get the distinct values as a list using list comprehension
+            data = [[value[0], value[1]] for value in data]
+            return data
+
+    def GetLastShareData(self):
+        with DBConnectionHandler() as db:
+            data = db.Section.query(Share.data)\
+                .order_by(Share.indice.desc()).first()
+            
+            return data[0]            
--- run.py
+++ run.py
@@ -2,6 +2,17 @@ from infra.Repository.Share_Repository import ShareRepository
 
 repo = ShareRepository()
 
-data = repo.Select()
+# data = repo.Select()
+# print(data)
 
+stocks_ticker = repo.SelectDistinctSymbols()
+# print(stocks_ticker)
+
+stock_and_name = repo.SelectDistinctSymbolsAndName()
+# print(stock_and_name)
+
+stock = repo.SelectOperationsFrom("BOVA11")
+# print(stock)
+
+data = repo.GetLastShareData()
 print(data)
