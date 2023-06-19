95cdf3434882da6f3b90a9cc38670b978a1864b9
Anderson Sachetto Rosa <sachetto@gmail.com>
2023-06-19 08:49:55 -0300

    Refactoring fields names in Stock class

--- infra/Entities/Stock.py
+++ infra/Entities/Stock.py
@@ -1,20 +1,23 @@
 from infra.Config.Base import Base
 from sqlalchemy import Column, Integer, String\
-    , Float
+    , Float, ForeignKey
+from sqlalchemy.orm import relationship
 
 # Define the class representing the table
 class Stock(Base):
     __tablename__ = 'stock'
 
     Id = Column(Integer, primary_key=True)
-    Ticker = Column(String)
-    Company = Column(String)
-    Price = Column(Float)
-    Category = Column(String)
+    Symbol = Column(String)
+    Company_Name = Column(String)
+    Current_Price = Column(Float)
+    Category_Id = Column(Integer, ForeignKey('Category.Id'))
+
+    Category = relationship("Category")
 
     def __repr__(self):
         return f"<Stock: Id='{self.Id}',\
-            Ticker='{self.Ticker}',\
-            Company='{self.Company}',\
-            Price='{self.Price}',\
+            Symbol='{self.Symbol}',\
+            Company_Name='{self.Company_Name}',\
+            Current_Price='{self.Current_Price}',\
             Category='{self.Category}'\n>"
