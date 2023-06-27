fda46c4ffd0519e4034b5da0fad7731ccbf780a2
Anderson Sachetto Rosa <sachetto@gmail.com>
2023-06-26 21:51:30 -0300

    First GUI (screen) navigation prototype using state machine

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
 from prompt_toolkit import prompt
-from prompt_toolkit.shortcuts import radiolist_dialog, input_dialog, button_dialog
+from prompt_toolkit.shortcuts import radiolist_dialog\
+    , input_dialog, button_dialog
 
 class AppState:
     def __init__(self):
         self.__stxtpos = 0
         self.__soppos = 1
+        self.__kofsc = 2
         self.state = 'screen1'
         self.handlers = {
             'screen1': self.handle_screen1,
@@ -14,12 +16,35 @@ class AppState:
             'screen5': self.handle_screen5,
         }
         self.screen_texts = {
-            'screen1': ['Select an option in Screen 1:', [('1', 'Option 1'), ('2', 'Option 2')]],
-            'screen2': ['Select an option in Screen 2:', [('1', 'Option 1'), ('2', 'Option 2')]],
-            'screen3': ['Select an option in Screen 3:', [('1', 'Option 1'), ('2', 'Option 2')]],
-            'screen4': ['Enter a value in Screen 4:'],
-            'screen5': ['Press OK to continue in Screen 5.'],
+            'screen1': ['Select an option in Screen 1:', [('1', 'Jump to screen 2'), ('2', 'Jump to screen 3')], self.radiolist_dialog],
+            'screen2': ['Select an option in Screen 2:', [('1', 'Back to screen 1'), ('2', 'Jump to screen 3')], self.radiolist_dialog],
+            'screen3': ['Select an option in Screen 3:', [('1', 'Back to screen 1'), ('2', 'Jump to screen 4')], self.radiolist_dialog],
+            'screen4': ['Enter a value in Screen 4:', [], self.input_dialog],
+            'screen5': ['Press OK to continue in Screen 5.', [('Continue', 'Continue'), ('Quit', 'Quit')], self.button_dialog],
         }
+
+    def radiolist_dialog(self, screen_options):
+        selected_option = radiolist_dialog(
+            title='Prompt Dialog',
+            text=screen_options[self.__stxtpos],
+            values=screen_options[self.__soppos],
+        ).run()
+        return selected_option
+    
+    def input_dialog(self, screen_options):
+        selected_option = input_dialog(
+            title='Prompt Dialog',
+            text=screen_options[self.__stxtpos],
+        ).run()
+        return selected_option
+
+    def button_dialog(self, screen_options):
+        selected_option = button_dialog(
+            title='Prompt Dialog',
+            text=screen_options[self.__stxtpos],
+            buttons=screen_options[self.__soppos],
+        ).run()
+        return selected_option
 
     def handle_input(self, user_input):
         # handler becomes a reference to the 
@@ -30,65 +55,46 @@ class AppState:
 
     def handle_screen1(self, user_input):
         if user_input == '1':
-            print('Option 1 selected in Screen 1')
-        elif user_input == '2':
-            print('Option 2 selected in Screen 1')
             self.state = 'screen2'
+        elif user_input == '2':
+            self.state = 'screen3'
 
     def handle_screen2(self, user_input):
         if user_input == '1':
-            print('Option 1 selected in Screen 2')
+            self.state = 'screen1'
         elif user_input == '2':
-            print('Option 2 selected in Screen 2')
             self.state = 'screen3'
 
     def handle_screen3(self, user_input):
         if user_input == '1':
-            print('Option 1 selected in Screen 3')
+            self.state = 'screen1'
         elif user_input == '2':
-            print('Option 2 selected in Screen 3')
             self.state = 'screen4'
 
     def handle_screen4(self, user_input):
         if user_input:
-            print(f'Input value in Screen 4: {user_input}')
-            self.state = 'screen5'
+            if user_input == 'restart':
+                self.state = 'screen1'
+            else:
+                self.state = 'screen5'
 
     def handle_screen5(self, user_input):
-        if user_input == 'OK':
-            print('OK button pressed in Screen 5')
+        if user_input == 'Continue':
             self.state = 'screen1'
+        else:
+            self.state = None
 
     def run(self):
-        while True:
-            handler = self.handlers.get(self.state)
+        while self.state != None:
+            screen_options = self.screen_texts.get(self.state)
+
+            handler = screen_options[self.__kofsc]
             if handler:
-                handler(None)  # Handle the screen transition
+                selected_option = handler(screen_options)  # Handle the screen
             else:
                 break
 
-            screen_text = self.screen_texts.get(self.state)
-            if self.state == 'screen4':
-                input_value = input_dialog(
-                    title='Prompt Dialog',
-                    text=screen_text[self.__stxtpos],
-                ).run()
-                self.handle_input(input_value)
-            elif self.state == 'screen5':
-                button_dialog(
-                    title='Prompt Dialog',
-                    text=screen_text[self.__stxtpos],
-                    buttons=[('OK', 'OK')],
-                ).run()
-                self.handle_input('OK')
-            else:
-                selected_option = radiolist_dialog(
-                    title='Prompt Dialog',
-                    text=screen_text[self.__stxtpos],
-                    values=screen_text[self.__soppos],
-                ).run()
-
-                self.handle_input(selected_option)
+            self.handle_input(selected_option)
 
 # Example usage
 app_state = AppState()
